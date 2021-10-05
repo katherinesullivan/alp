44b818941cedf31677d16d720d2ef35205132394
Ines Cipullo <inescipullo@gmail.com>
2021-10-05 16:01:15 -0300

    //

--- TP2/src/Untyped.hs
+++ TP2/src/Untyped.hs
@@ -5,6 +5,7 @@ import           Data.List
 import           Data.Maybe
 
 import           Common
+import StringBuffer (StringBuffer(len))
 
 ----------------------------------------------
 -- Sección 2  
@@ -35,7 +36,8 @@ lookforAux (x:xs) var idx = if var == x then idx else lookforAux xs var (idx+1)
 -------------------------------
 
 vapp :: Value -> Value -> Value
-vapp = undefined
+vapp (VLam abs) val = abs val
+vapp (VNeutral neu) val = VNeutral (NApp neu val)
 
 eval :: NameEnv Value -> Term -> Value
 eval e t = eval' t (e, [])
@@ -50,13 +52,58 @@ eval' _          _         = undefined
 -- Ejercicio 5
 --------------------------------
 
+-- quote :: Value -> Term
+-- quote val = quoteAux val 0
+
+-- quoteAux :: Value -> Int -> Term
+-- quoteAux (VNeutral (NFree var)) idx = Free var
+-- quoteAux (VNeutral (NApp neu val)) idx = quoteAux (VNeutral neu) idx :@: quoteAux val idx
+-- quoteAux (VLam fun) idx = let term = quoteAux (fun (VNeutral (NFree (Quote idx)))) (idx+1)
+--                           in Lam (replace term)
+
+-- replace :: Term -> Term
+-- replace (Free (Quote k)) = Bound (i - k - 1) -- me falta el i
+-- replace x = x
+
+
 quote :: Value -> Term
-quote (VNeutral (NFree var)) = Free var
-quote (VNeutral (NApp neu val)) = quote (VNeutral neu) :@: quote val
-quote (VLam fun) = undefined
+quote val = fst (quote2 val 0)
+
+
+quote2 :: Value -> Int -> (Term, Int)
+quote2 (VNeutral (NFree var)) idx = (Free var, idx)
+quote2 (VNeutral (NApp neu val)) idx = (fst (quote2 (VNeutral neu) idx) :@: fst (quote2 val idx), idx)
+quote2 (VLam fun) idx = let (term, total) = quote2 (fun (VNeutral (NFree (Quote idx)))) (idx+1)
+                        in (Lam (replace term total), total)
+
+
+replace :: Term -> Int -> Term
+replace (Free (Quote k)) i = Bound (i - k - 1)
+replace x _ = x
+
+
+-- ejemplo :: Value -> Value 
+-- -- ejemplo (VLam fun) = VLam (\ x -> VNeutral (NApp (x) (VLam (\ y -> VNeutral (NApp (x) y)))))
+-- ejemplo (VNeutral x) = VNeutral (NApp x (VLam (ejemplo2 x)))
+
+
+-- ejemplo2 :: Neutral -> Value -> Value 
+-- ejemplo2 x y = VNeutral (NApp x y)
 
 
 
 
+esPrimo :: Int -> Bool 
+esPrimo n = [x | x <- [1..n], mod n x == 0] == [1,n]
 
 
+
+
+isprime :: Int -> Bool
+isprime 1 = False
+isprime 2 = True
+isprime n = isprimerec n (n-1)
+
+isprimerec :: Int -> Int -> Bool
+isprimerec _ 1 = False
+isprimerec n t = if (n `rem` t) == 0 then False else isprimerec n (n-1)
